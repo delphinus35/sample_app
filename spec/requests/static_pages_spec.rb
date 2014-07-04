3ba965657714b9fcb09a194b404bc127d0cf263e
JINNOUCHI Yasushi <jinnouchi.yasushi@dena.jp>
2014-07-04 18:34:09 +0900

    tutorial 5.3.0

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,4 +39,16 @@ RSpec.describe "StaticPages", :type => :request do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+  describe 'Contact page' do
+    it 'should have the content "Contact"' do
+      visit '/static_pages/contact'
+      expect(page).to have_content 'Contact'
+    end
+
+    it 'should have the title "Contact"' do
+      visit '/static_pages/contact'
+      expect(page).to have_title "#{base_title} | Contact"
+    end
+  end
 end
